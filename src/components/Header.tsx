fcf928e6df84ed29a027ca8c0a0183aa815306dd
uuronn <uuronprogram@gmail.com>
2024-01-02 18:21:09 +0900

    Create: Headerコンポーネントを作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  breadcrumbList: { title: string; path: string }[];
+};
+
+export const Header = ({ breadcrumbList }: Props) => {
+  return (
+    <header className="fixed z-10 w-screen border-b-2 border-black bg-white px-6 py-3">
+      <ul className="flex">
+        {breadcrumbList.map((breadcrumb, i) => (
+          <li className="text-lg" key={i}>
+            <a href={breadcrumb.path} className="text-lg font-bold">
+              {breadcrumb.title}
+            </a>
+            {i < breadcrumbList.length - 1 && (
+              <span className="mx-2 text-xl font-normal">{">"}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
