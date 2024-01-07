f15c9595fbd7c0233f88a4658998585b51e129ae
uuronn <uuronprogram@gmail.com>
2024-01-08 01:14:40 +0900

    リポジトリ名変更テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     (async () => {
-      const test = await fetch("http://localhost:3000/cats");
+      const test = await fetch("http://localhost:3000/blogs");
       const cat = await test.json();
       console.log("aaa", cat);
     })();
