032181af7e900e851397e78cab3c834618a5cf57
uuronn <uuronprogram@gmail.com>
2024-01-10 08:23:42 +0900

    認可処理を追加しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { Header } from "~/components/Header";
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Header } from '~/components/Header';
+import { BlogCard } from './components/BlogCard';
 
 export type Blog = {
   id: string;
@@ -11,25 +12,41 @@ export type Blog = {
 };
 
 export default function Home() {
-  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [blogs, setBlogs] = useState<Blog[]>();
 
   useEffect(() => {
     (async () => {
-      const res = await fetch("http://localhost:8000/blogs");
+      const res = await fetch('http://localhost:8000/blogs', {
+        headers: {
+          Authorization: process.env.NEXT_PUBLIC_AUTHORIZATION_KEY as string,
+        },
+      });
       const blogs = await res.json();
       setBlogs(blogs);
     })();
   }, []);
 
+  if (!blogs)
+    return (
+      <main>
+        <Header breadcrumbList={[{ title: 'いっせいブログ', path: '' }]} />
+        <div>...loading</div>
+      </main>
+    );
+
   return (
     <>
-      <Header breadcrumbList={[{ title: "いっせいブログ", path: "/" }]} />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {blogs.map((blog) => (
-          <Link href={blog.id} key={blog.id}>
-            {blog.title}
-          </Link>
-        ))}
+      <Header breadcrumbList={[{ title: 'いっせいブログ', path: '' }]} />
+      <main className="p-6">
+        <ul className="flex flex-wrap justify-center gap-3">
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Link href={blog.id}>
+                <BlogCard blog={blog} />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
