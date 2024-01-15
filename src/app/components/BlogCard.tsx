9d12502144c48ec48d0d4971bbf8abbbb954f9a9
uuronn <uuronprogram@gmail.com>
2024-01-16 06:22:29 +0900

    Fix: import

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { ComponentPropsWithoutRef } from 'react';
-import { Blog } from '~/app/page';
+import { Blog } from '~/constant/types';
 
 type Props = {
   blog: Blog;
