facf1d8bb5bfca83dd01ff40c83a96a3ec24f045
uuronn <uuronprogram@gmail.com>
2024-01-16 05:17:43 +0900

    Fix: blogsにblogページが来るように修正

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,14 +1,28 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { Blog } from '../page';
+import { useContext, useEffect, useState } from 'react';
+import { Blog } from '~/constant/types';
 import { Header } from '~/components/Header';
 import { BASE_URL } from '~/constant';
+import { AuthContext } from '~/contexts/AuthContext';
+import { useFetchLikesCount } from '~/hooks/useFetchLikesCount';
 
 export default function Blog() {
   const [blog, setBlog] = useState<Blog>();
   const router = useParams();
+  const { authUser, isLoad } = useContext(AuthContext);
+
+  const count = useFetchLikesCount(router.blogId as string);
+
+  const likeHandler = () => {
+    console.log('authuser', authUser);
+
+    if (!authUser) return;
+
+    const userId = authUser.uid;
+    // setBlog((prev) => console.log('prev', prev));
+  };
 
   useEffect(() => {
     (async () => {
@@ -30,6 +44,9 @@ export default function Blog() {
       />
       <h1>{blog.title}</h1>
       <p>{blog.content}</p>
+      <p>いいね:{count}</p>
+
+      <button onClick={likeHandler}>ハート</button>
     </div>
   );
 }
