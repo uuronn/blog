cc50b597d6f0afe3e48f07b31c625b71737ea2c4
uuronn <uuronprogram@gmail.com>
2024-01-17 23:54:25 +0900

    fix

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { ComponentPropsWithoutRef } from 'react';
+import { LikeIcon } from '~/components/icons/LikeIcon';
 import { Blog } from '~/constant/types';
 
 type Props = {
@@ -10,12 +11,16 @@ export const BlogCard = ({ blog, className }: Props) => {
   return (
     <div
       className={classNames(
-        'h-32 w-96 rounded-2xl border-2 border-black p-4',
+        'relative h-32 w-96 rounded-2xl border-2 border-black p-4',
         className,
       )}
     >
-      <p>{blog.title}</p>
+      <p className="mb-2 text-lg font-bold">{blog.title}</p>
       <p>{blog.content}</p>
+      <p className="absolute bottom-[16px] right-[16px] flex items-center gap-2">
+        {blog.likes.length}
+        <LikeIcon />
+      </p>
     </div>
   );
 };
