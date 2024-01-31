5d3a54d0994bea1c8c19a9c7f5f7b85eb996ddca
uuronn <uuronprogram@gmail.com>
2024-02-01 07:44:46 +0900

    Fix: 不要なコードを削除

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,6 @@
 'use client';
 
 import { Header } from '~/components/Header';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-// import { auth } from '~/firebase/app';
-import { BASE_URL } from '~/constant';
-// import { useContext } from 'react';
-// import { AuthContext } from '~/contexts/AuthContext';
-import { useRouter } from 'next/navigation';
 
 export type Blog = {
   id: string;
@@ -16,24 +9,7 @@ export type Blog = {
 };
 
 export default function Home() {
-  // const { authUser, isLoad } = useContext(AuthContext);
-  const router = useRouter();
-  const provider = new GoogleAuthProvider();
-
-  const onClick = async () => {
-    // const res = await signInWithPopup(auth, provider);
-    // const userId = res.user.uid;
-    // const name = res.user.displayName || '';
-    // const email = res.user.email || '';
-    // const token = await res.user.getIdToken();
-    // await fetch(`${BASE_URL}/users`, {
-    //   headers: {
-    //     Authorization: 'Bearer ' + token,
-    //   },
-    // });
-  };
-
-  // if (authUser) router.push('/');
+  const onClick = async () => {};
 
   return (
     <>
