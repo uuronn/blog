ad1eb5f6531e34a3de2e2668e25da98480dc9dc3
uuronn <uuronprogram@gmail.com>
2024-02-01 07:58:53 +0900

    FIx: AppRouterを活かすためにコンポーネントを分けました

---
+++ src/app/[blogId]/_components/BlogDetail.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import { LikeIcon } from '~/components/icons/LikeIcon';
+import { BASE_URL } from '~/constant';
+import { Blog } from '~/constant/types';
+
+export const BlogDetail = () => {
+  const { blogId } = useParams();
+
+  const [blog, setBlog] = useState<Blog>();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${BASE_URL}/blog/${blogId}`);
+
+      const blog = await res.json();
+
+      setBlog(blog);
+    })();
+  }, []);
+
+  if (!blog) return <div>loading...</div>;
+
+  const date = new Date(blog.createdAt);
+
+  const dateText = `${date.getFullYear()}年${
+    date.getMonth() + 1
+  }月${date.getDate()}日`;
+
+  return (
+    <>
+      <h1>{blog.title}</h1>
+      <p>{blog.content}</p>
+      <p>投稿日 {dateText}</p>
+      <button className="flex items-center gap-2">
+        <LikeIcon />
+      </button>
+    </>
+  );
+};
