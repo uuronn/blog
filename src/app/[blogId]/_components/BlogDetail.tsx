5b0c500067550e971d836fb7a0f750fbc1a8cf9c
uuronn <uuronprogram@gmail.com>
2024-02-20 06:48:54 +0900

    Fix: addlike

--- src/app/[blogId]/_components/BlogDetail.tsx
+++ src/app/[blogId]/_components/BlogDetail.tsx
@@ -6,10 +6,23 @@ import { LikeIcon } from '~/components/icons/LikeIcon';
 
 import { formatDate } from '~/utils/formatDate';
 import { useFetchBlog } from '../hooks/useFetchBlog';
+import { BASE_URL } from '~/constant';
 
 export const BlogDetail = () => {
   const { blogId } = useParams<{ [key: string]: string }>();
   const { blog, isLoading } = useFetchBlog(blogId);
+
+  const addLike = () => {
+    fetch(`${BASE_URL}/like`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        blogId,
+      }),
+    });
+  };
 
   if (isLoading) return <div>loading...</div>;
 
@@ -20,7 +33,7 @@ export const BlogDetail = () => {
       <h1 className="text-3xl font-bold">{blog.title}</h1>
       <p className="text-right">投稿日 {formatDate(blog.createdAt)}</p>
       <p>{blog.content}</p>
-      <button className="flex items-center gap-2">
+      <button onClick={addLike} className="flex items-center gap-2">
         <LikeIcon />
       </button>
     </div>
